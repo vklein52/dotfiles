388ae152dff7ab9e2273deec79ac215147daf0b5
Vijay Klein <uytre7907@gmail.com>
2020-11-12 15:44:47 -0600

    Added some more scripts as well as a starter script that can be used through curl

---
+++ scripts/install_packages.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# This script is intended to be a monolith script that creates a configuration with various specifications.
+# First, it ensures Python and PIP are installed, and uses PIP to install Ansible.
+# Next, an Ansible playbook is kicked off, targetting localhost, using the package module to install essentials such as emacs and zsh
+# Lastly, it clones the git repo (TODO)
+
+function determine-os () {
+    UNAME_OUT=`uname -a`
+    for OS in "MANJARO" "ARCH" "UBUNTU"; do
+        if grep -i "$OS" <(echo "$UNAME_OUT"); then
+            echo "$OS" 1>&2
+        fi
+    done
+}
+
+function install-python-and-pip () {
+    OS=`determine-os 2>&1 >/dev/null`
+    if [ "$OS" == "MANJARO" ] || [ "$OS" == "ARCH" ]; then
+        sudo pacman -Syu --noconfirm python python-pip
+    elif [ "$OS" == "UBUNTU" ]; then
+        sudo apt-get update -y
+        sudo apt-get install python3 python3-pip -y
+    else
+        echo "Unsupported OS: ${OS}"       
+    fi
+}
+
+function install-ansible () {
+    install-python-and-pip
+    python -m pip install --user ansible
+}
+
+function run-ansible-playbook () {
+    install-ansible
+    ~/.local/bin/ansible-playbook ansible/site.yaml
+}
+
+# install-python-and-pip
+run-ansible-playbook
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,40 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# This script is intended to be a monolith script that creates a configuration with various specifications.
-# First, it ensures Python and PIP are installed, and uses PIP to install Ansible.
-# Next, an Ansible playbook is kicked off, targetting localhost, using the package module to install essentials such as emacs and zsh
-# Lastly, it clones the git repo (TODO)
-
-function determine-os () {
-    UNAME_OUT=`uname -a`
-    for OS in "MANJARO" "ARCH" "UBUNTU"; do
-        if grep -i "$OS" <(echo "$UNAME_OUT"); then
-            echo "$OS" 1>&2
-        fi
-    done
+function clone-repo () {
+    pushd ~
+    git init
+    git remote add https://github.com/vklein52/dotfiles.git
+    git pull
+    popd
 }
 
-function install-python-and-pip () {
-    OS=`determine-os 2>&1 >/dev/null`
-    if [ "$OS" == "MANJARO" ] || [ "$OS" == "ARCH" ]; then
-        sudo pacman -Syu --noconfirm python python-pip
-    elif [ "$OS" == "UBUNTU" ]; then
-        sudo apt-get update -y
-        sudo apt-get install python3 python3-pip -y
-    else
-        echo "Unsupported OS: ${OS}"       
-    fi
+# Assumes repo has been cloned in the home directory
+function install-packages () {
+    pushd ~/scripts
+    ./install_packages.sh
+    popd
 }
 
-function install-ansible () {
-    install-python-and-pip
-    python -m pip install --user ansible
+function setup () {
+    clone-repo
+    install-packages
 }
 
-function run-ansible-playbook () {
-    # install-ansible
-    ~/.local/bin/ansible-playbook install_packages/site.yaml
-}
-
-# install-python-and-pip
-run-ansible-playbook
+setup
